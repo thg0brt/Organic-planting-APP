b4f235846f7a3dba99b1334926765c98f1375e60
thg0brt <thiago.13.brito@gmail.com>
2018-11-16 15:35:13 -0200

    alface e couve-flor add

---
+++ MyApplication3/app/src/main/java/com/example/mathe/myapplication/alface.java
@@ -0,0 +1,42 @@
+package com.example.mathe.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+public class alface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alface);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setTitle("Voltar às hortaliças");
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                startActivity(new Intent(this, Houtono.class));
+                finishAffinity();
+                break;
+            default:break;
+        }
+        return true;
+    }
+    @Override
+    public void onBackPressed(){
+        startActivity(new Intent(this, Houtono.class));
+        finishAffinity();
+        return;
+    }
+    // -------------------------------------------------------------------------------------------------------------
+    public void VoltarTela(View view){
+        Intent voltar = new Intent(getApplicationContext(),Houtono.class);
+        startActivity(voltar);
+        finish();
+    }
+}
